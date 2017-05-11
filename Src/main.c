1d3f2db1329b8b5220a36362b78af790e9d614d9
XVincentX <vincenzo@apiary.io>
2017-05-11 16:51:20 +0200

    Make flash the default build command

--- Src/main.c
+++ Src/main.c
@@ -2,15 +2,12 @@
 
 int main(void)
 {
-  RCC->AHBENR = 1 << 18;
-  GPIOB->MODER = 1 << 16 | 1 << 12 | 1 << 14 | 1 << 18;
+  RCC->AHBENR = 1 << 18 | 1 << 17; // Clock on GPIOB and A
+  GPIOB->MODER = 1 << 16 | 1 << 12 | 1 << 14 | 1 << 18; //Set port 8,6,7,9 to input
+  GPIOA->MODER = 0; // Set port 0 as input
 
   while(1) {
-    GPIOB->ODR = 1 << 6 | 1 << 8 | 1 << 9 | 1 << 7;
-    GPIOB->ODR = 1 << 6 | 1 << 8 | 1 << 9;
-    GPIOB->ODR = 1 << 6 | 1 << 8;
-    GPIOB->ODR = 1 << 6;
-    GPIOB->ODR = 1 << 6 | 1 << 8 | 1 << 9 | 1 << 7;
-    GPIOB->ODR = 0;
+    if (GPIOA->IDR & 1)
+      GPIOB->ODR = 1 << 6 | 1 << 8 | 1 << 9 | 1 << 7;
   }
 }
