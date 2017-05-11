bb6383d4fa9de05ef19735dc369612c2287db00e
XVincentX <vincenzo@apiary.io>
2017-05-11 14:37:49 +0200

    Added tasks

--- Src/main.c
+++ Src/main.c
@@ -3,9 +3,11 @@
 int main(void)
 {
   RCC->AHBENR = 1 << 18;
-  GPIOB->MODER = 1 << 16;
+  GPIOB->MODER = 1 << 16 | 1 << 12 | 1 << 14 | 1 << 18;
 
-  GPIOB->ODR = 1 << 8;
 
-  while(1);
+  while(1) {
+    GPIOB->ODR = 1 << 6 | 1 << 8 | 1 << 9 | 1 << 7;
+    GPIOB->ODR = 0;
+  }
 }
