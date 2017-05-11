b4b61c340a1b3379db254e61549dea96f59f1d19
XVincentX <vincenzo@apiary.io>
2017-05-11 17:05:26 +0200

    Better example: one led per time.

--- Src/main.c
+++ Src/main.c
@@ -2,12 +2,27 @@
 
 int main(void)
 {
-  RCC->AHBENR = 1 << 18 | 1 << 17; // Clock on GPIOB and A
+  RCC->AHBENR = 1 << 18 | 1 << 17;                      // Clock on GPIOB and A
   GPIOB->MODER = 1 << 16 | 1 << 12 | 1 << 14 | 1 << 18; //Set port 8,6,7,9 to input
-  GPIOA->MODER = 0; // Set port 0 as input
+  GPIOA->MODER &= 0;                                    // Set port 0 as input
 
-  while(1) {
-    if (GPIOA->IDR & 1)
-      GPIOB->ODR = 1 << 6 | 1 << 8 | 1 << 9 | 1 << 7;
+  const unsigned int leds[] = {
+      1 << 6,
+      1 << 7,
+      1 << 8,
+      1 << 9,
+  };
+
+  unsigned int index = 0;
+
+  while (1)
+  {
+    if (GPIOA->IDR & GPIO_IDR_0)
+    {
+      GPIOB->ODR = leds[index % 4];
+      while (GPIOA->IDR & GPIO_IDR_0)
+        ;
+      index++;
+    }
   }
 }
