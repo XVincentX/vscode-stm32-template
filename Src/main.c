356954da8a5bf3fc8619a77f6323f4d5032a7fee
XVincentX <vincenzo@apiary.io>
2017-05-12 15:30:03 +0200

    Modify the example for 303X

--- Src/main.c
+++ Src/main.c
@@ -2,15 +2,19 @@
 
 int main(void)
 {
-  RCC->AHBENR = 1 << 18 | 1 << 17;                      // Clock on GPIOB and A
-  GPIOB->MODER = 1 << 16 | 1 << 12 | 1 << 14 | 1 << 18; //Set port 8,6,7,9 to input
-  GPIOA->MODER &= 0;                                    // Set port 0 as input
+  RCC->AHBENR = RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOEEN;                                        // Clock on GPIOE and A
+  GPIOE->MODER = 1 << 18 | 1 << 16 | 1 << 20 | 1 << 30 | 1 << 22 | 1 << 28 | 1 << 24 | 1 << 26; //Set port 8,6,7,9 to input
+  GPIOA->MODER &= ~GPIO_MODER_MODER0;                                                           // Set port 0 as input
 
   const unsigned int leds[] = {
-      1 << 6,
-      1 << 7,
-      1 << 8,
       1 << 9,
+      1 << 8,
+      1 << 10,
+      1 << 15,
+      1 << 11,
+      1 << 14,
+      1 << 12,
+      1 << 13,
   };
 
   unsigned int index = 0;
@@ -19,7 +23,7 @@ int main(void)
   {
     if (GPIOA->IDR & GPIO_IDR_0)
     {
-      GPIOB->ODR = leds[index % 4];
+      GPIOE->ODR = leds[index % 8];
       while (GPIOA->IDR & GPIO_IDR_0)
         ;
       index++;
